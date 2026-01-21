18a47ba0b2fedbf08c4eb14d0de09772ba439758
Ezidiegwu Nnenna <ezidiegwunnennafavour@gmail.com>
2026-01-21 21:47:04 +0100

    Add MultiplicationTable: prints table of given number using BufferedReader and for loop

---
+++ src/conditionals/MultiplicationTable.java
@@ -0,0 +1,19 @@
+package conditionals;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MultiplicationTable {
+    public static void main(String args[]) throws IOException {
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(in);
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int num=1;num<=10;num++) {
+            int result = N * num;
+            System.out.println(N + " x " + num + " = " + result);
+        }
+    }
+}
